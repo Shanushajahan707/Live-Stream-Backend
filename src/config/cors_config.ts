474a83df9deb93c5b3f1e5165c0d90d947cbc43d
Shanushajahan707 <shanushajahan707@gmail.com>
2024-07-04 18:32:01 +0530

    changes in cors cofiguration 2

--- src/config/cors_config.ts
+++ src/config/cors_config.ts
@@ -2,5 +2,5 @@ export  const corsOptions = {
     origin: 'http://localhost:4200', // Allow all origins
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    credentials: true  
 };
