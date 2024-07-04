f85dfdf5114b926d148b4ff5ffc26113ba17e4f5
Shanushajahan707 <shanushajahan707@gmail.com>
2024-07-04 16:15:36 +0530

    cors option changed 3

--- src/config/cors_config.ts
+++ src/config/cors_config.ts
@@ -1,7 +1,6 @@
 export const corsOption = {
-    origin: "http://localhost:4200",
+    origin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', "Access-Control-Allow-Origin"],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
   };
-  
